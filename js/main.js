0090b3a92caccf9523f986fa11dede2af364968d
voldergit <voldergit@gmail.com>
2022-07-05 16:29:56 +0300

    sections items+works, jquery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-$(function(){
-  $('.slider__inner').slick({
-      arrows: false,
-      dots: true,
+$(function () {
+  $(".slider__inner").slick({
+    arrows: false,
+    dots: true,
+  });
+  $(".works__inner").slick({
+    // arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/sliders/slide-left.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/sliders/slide-right.svg" alt=""></button>',
   });
 });
